b923a08560411de64fca8ec20096ff5863c0d0c8
Bastien <jugipalace@gmail.com>
2021-10-28 23:59:57 +0900

    first commit after simple login activity

--- app/src/main/java/com/example/findme/MainActivity.java
+++ app/src/main/java/com/example/findme/MainActivity.java
@@ -1,7 +1,22 @@
 package com.example.findme;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("NAME");
+        Toast toast = Toast.makeText(getBaseContext(), "username is " + username, Toast.LENGTH_LONG);
+        toast.show();
+    }
 }
