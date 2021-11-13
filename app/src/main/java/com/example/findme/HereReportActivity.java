fd6b2525f45fa293efac7ddf85f469f05fbbe70c
kadiace <rlacjfghks95@gmail.com>
2021-11-13 17:41:49 +0900

    connect buttons

--- app/src/main/java/com/example/findme/HereReportActivity.java
+++ app/src/main/java/com/example/findme/HereReportActivity.java
@@ -1,12 +1,16 @@
 package com.example.findme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class HereReportActivity extends Activity {
     @Override
@@ -14,22 +18,42 @@ public class HereReportActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.here);
 
-//        Button imageButton = findViewById(R.id.camera_layout);
-//        Button submitButton = findViewById(R.id.submit_button);
-//
-//        imageButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast toast = Toast.makeText(getBaseContext(), "gallery", Toast.LENGTH_LONG);
-//                toast.show();
-//            }
-//        });
-//        submitButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast toast = Toast.makeText(getBaseContext(), "submit", Toast.LENGTH_LONG);
-//                toast.show();
-//            }
-//        });
+        final TextView back_button = findViewById(R.id.back);
+        final CardView location_button = findViewById(R.id.location_card);
+        final ConstraintLayout image_button = findViewById(R.id.camera_layout);
+        final Button submit_button = findViewById(R.id.submit_button);
+
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PetInfoActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        location_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getBaseContext(), "location", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getBaseContext(), "gallery", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getBaseContext(), "submit", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 }
