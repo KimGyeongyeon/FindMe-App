759d5ec3e6486c11f45e6a4c31995163ecefaa9d
KimGyeongyeon <kgy556@kaist.ac.kr>
2021-11-16 17:46:50 +0900

    PetInfo is connected with Firebase

--- app/src/main/java/com/example/findme/PetInfo.java
+++ app/src/main/java/com/example/findme/PetInfo.java
@@ -4,29 +4,89 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class PetInfo {
-    private String name;
-    private Date date;
-    private String detail;
-    private String[] location = {"36","127"};
+    private String date;
     private String[] profile_images = {"a","a","a","a"};
+    private double[] location = {36.0, 127.0};
+    private String name;
+    private String detail;
 
-    public PetInfo(){}
+
+    public PetInfo(){
+
+    }
+
+    public PetInfo(PetInfo p){
+        this.date = p.getDate();
+        this.name = p.getName();
+        this.detail = p.getDetail();
+        this.location[0] = p.getLocation()[0];
+        this.location[1] = p.getLocation()[1];
+        for(int i=0 ; i<4; i++){
+            this.profile_images[i] = p.getImage()[i];
+        }
+    }
+
+    public PetInfo( Date date, String[] profile_images,
+                    double[] location, String name, String detail){
+        super();
+        this.date = String.valueOf(date);
+        this.name = name;
+        this.detail = detail;
+        this.location[0] = location[0];
+        this.location[1] = location[1];
+        for(int i=0 ; i<4; i++){
+            this.profile_images[i] = profile_images[i];
+        }
+
+    }
+
+    public PetInfo sample_data(){
+        Date test_date = new Date(2021, 10, 14, 12, 27, 31);
+        String[] url = {
+                "https://firebasestorage.googleapis.com/v0/b/findme-a2f27.appspot.com/o/pet-info%2FGold%2Fgold_fail_casejpg.jpg?alt=media&token=b74d2b08-bddf-4b25-a241-c23f6237216a",
+                "https://firebasestorage.googleapis.com/v0/b/findme-a2f27.appspot.com/o/pet-info%2FGold%2Fgold_fail_casejpg.jpg?alt=media&token=b74d2b08-bddf-4b25-a241-c23f6237216a",
+                "https://firebasestorage.googleapis.com/v0/b/findme-a2f27.appspot.com/o/pet-info%2FGold%2Fgold_fail_casejpg.jpg?alt=media&token=b74d2b08-bddf-4b25-a241-c23f6237216a",
+                "https://firebasestorage.googleapis.com/v0/b/findme-a2f27.appspot.com/o/pet-info%2FGold%2Fgold_fail_casejpg.jpg?alt=media&token=b74d2b08-bddf-4b25-a241-c23f6237216a"
+        };
+        double[] loc_arr = {36.368155, 127.364914};
+        return new PetInfo(test_date, url, loc_arr,
+                "Guemi","I lost him in front of E3. He is wearing red ribbon. Please find himTT ");
+    }
 
     public String getName(){
         return name;
     }
 
+    public void setName(){
+        this.name = name;
+    }
+
     public String getDate(){
         SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
-        return format.format(date);
+//        Date tmp = format.format(date)
+//        return format.format(date);
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = String.valueOf(date);
     }
 
     public String getDetail(){
         return detail;
     }
 
-    public String[] getLocation(){
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public double[] getLocation(){
         return location;
+    }
+
+    public void setLocation(float[] location) {
+        this.location[0] = location[0];
+        this.location[1] = location[1];
     }
 
     public String[] getImage(){
@@ -34,5 +94,9 @@ public class PetInfo {
         return profile_images;
     }
 
-
+    public void setProfile_images(String[] profile_images) {
+        for(int i = 0 ; i<4 ; i++) {
+            this.profile_images[i] = profile_images[i];
+        }
+    }
 }
--- app/src/main/java/com/example/findme/PetInfoActivity.java
+++ app/src/main/java/com/example/findme/PetInfoActivity.java
@@ -1,48 +1,119 @@
 package com.example.findme;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 public class PetInfoActivity extends AppCompatActivity {
 
-    private ArrayList<PetInfo> PetArray;
-    private Context context;
+    private ArrayList<PetInfo> petArray;
+    private ArrayList<ImageView> images;
+    // private Context context;
+    private FirebaseDatabase database;
+    private DatabaseReference ref;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 1) View setting
         setContentView(R.layout.petinfo);
-
         final TextView name = findViewById(R.id.pet_name_here);
         final TextView date = findViewById(R.id.pet_date_here);
         final TextView detail = findViewById(R.id.pet_detail_here);
-        final ArrayList<ImageView> images = new ArrayList<ImageView>();
+        images = new ArrayList<>();
         images.add(findViewById(R.id.imageView_1));
         images.add(findViewById(R.id.imageView_2));
         images.add(findViewById(R.id.imageView_3));
         images.add(findViewById(R.id.imageView_4));
 
-        //이걸 firebase와 연동.
-        for(ImageView i : images){
-            i.setImageResource(R.drawable.dog);
-            //PetArray.getImage()로 url array(array list 아님!) 받기
+        // 2) Initialize FireBase-related variables
+        petArray = new ArrayList<>(); //Include Every Pet information
+        database = FirebaseDatabase.getInstance(); // Connect FireBase Here
+        ref = database.getReference("pet");// Connect DB table
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                petArray.clear();
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    //PetInfo object 형태로 값 저장
+                    PetInfo info = dataSnapshot.getValue(PetInfo.class);
+                    petArray.add(info);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("PetInfoActivity", String.valueOf(error.toException()));
+            }
+        });
+
+
+
+         // 3) Distribute the info to view
+
+        // 요청한 pet의 정보를 찾고 그에 해당하는 index를 설정하는 방식으로 변경하자.
+//        try{
+//            int right_index = 0;
+//            PetInfo the_pet = new PetInfo(petArray.get(right_index));
+//
+//        }catch (java.lang.Exception exe){
+//
+//            Log.d("PetInfo-81", String.valueOf(petArray.size()));
+//            Log.d("PetInfo-81",String.valueOf(exe));
+//
+//        }
+
+        try{
+            int num = 0;
+            for(ImageView i : images){
+                Glide.with(i).load(petArray.get(0).getImage()[num])
+                        .into(i);
+//            i.setImageResource(R.drawable.dog);
+                //petArray.getImage()로 url array(array list 아님!) 받기
+                num++;
+            }
+        }catch (java.lang.Exception ex){
+            Log.d("PetInfo-99", String.valueOf(ex));
         }
+
+        try{
+            name.setText(petArray.get(0).getName());
+            date.setText(petArray.get(0).getDate());
+            detail.setText(petArray.get(0).getDetail());
+        }catch (java.lang.Exception ex){
+            Log.d("PetInfo-103", String.valueOf(ex));
+        }
+
+
+
+
+
 
         /*
          *
-         *   Click Event
+         *   a ) Click Event
          *
          */
         final Button main_button = findViewById(R.id.main_bar);
